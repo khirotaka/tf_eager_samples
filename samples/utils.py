bf15a10f71d02f6fa5a8b0774602015ca55bb807
KawashimaHirotaka <k.hirotaka@me.com>
2018-09-13 20:40:45 +0900

    Support GPU acceleration.

--- samples/utils.py
+++ samples/utils.py
@@ -24,34 +24,36 @@ def grad(model, x, y, training=False):
     return tape.gradient(loss_value, model.variables)
 
 
-def train(model, optimizer, dataset, epochs):
-    for e in range(epochs):
-        epoch_loss_avg = tfe.metrics.Mean()
-        train_accuracy = tfe.metrics.Accuracy()
+def train(model, optimizer, dataset, epochs, device="cpu:0"):
+    with tf.device(device):
+        for e in range(epochs):
+            epoch_loss_avg = tfe.metrics.Mean()
+            train_accuracy = tfe.metrics.Accuracy()
 
-        x, y = iter(dataset).next()
-        for (i, (x, y)) in enumerate(dataset):
-            grads = grad(model, x, y, training=True)
-            optimizer.apply_gradients(zip(grads, model.variables), global_step=tf.train.get_or_create_global_step())
-            train_accuracy(tf.argmax(model(x), axis=1, output_type=tf.int32),
+            x, y = iter(dataset).next()
+            for (i, (x, y)) in enumerate(dataset):
+                grads = grad(model, x, y, training=True)
+                optimizer.apply_gradients(zip(grads, model.variables), global_step=tf.train.get_or_create_global_step())
+                train_accuracy(tf.argmax(model(x), axis=1, output_type=tf.int32),
                            tf.argmax(y, axis=1, output_type=tf.int32))
 
-            if i % 200 == 0:
-                print("Loss: {:.4f} - Acc: {:.4f}".format(epoch_loss_avg(loss(model, x, y)), train_accuracy.result()))
+                if i % 200 == 0:
+                    print("Loss: {:.4f} - Acc: {:.4f}".format(epoch_loss_avg(loss(model, x, y)), train_accuracy.result()))
 
-        print("-"*50)
-        print("Epochs {} / {} | Loss: {:.4f} - Accuracy: {:.3%}".format(
-            e + 1, epochs, epoch_loss_avg(loss(model, x, y)), train_accuracy.result()
-        ))
+            print("-"*50)
+            print("Epochs {} / {} | Loss: {:.4f} - Accuracy: {:.3%}".format(
+                e + 1, epochs, epoch_loss_avg(loss(model, x, y)), train_accuracy.result()
+            ))
 
 
-def test(model, dataset):
-    test_accuracy = tfe.metrics.Accuracy()
-    for (x, y) in dataset:
-        test_accuracy(tf.argmax(model(x), axis=1, output_type=tf.int32),
-                      tf.argmax(y, axis=1, output_type=tf.int32))
+def test(model, dataset, device="cpu:0"):
+    with tf.device(device):
+        test_accuracy = tfe.metrics.Accuracy()
+        for (x, y) in dataset:
+            test_accuracy(tf.argmax(model(x), axis=1, output_type=tf.int32),
+                          tf.argmax(y, axis=1, output_type=tf.int32))
 
-    print("Test set accuracy: {:.3%}".format(test_accuracy.result()))
+        print("Test set accuracy: {:.3%}".format(test_accuracy.result()))
 
 
 def predict(model, x):
